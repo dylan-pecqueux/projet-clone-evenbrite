4b933051c076e9fea5b292206c7b83791082ec39
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2020-11-03 11:32:47 +0100

    2.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
     has_many :admin_events, foreign_key: 'admin_id', class_name: 'Event'
     has_many :attendances
     has_many :participant_events, foreign_key: 'participant_id', class_name: "Event"
